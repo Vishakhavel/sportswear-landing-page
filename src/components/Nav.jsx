f8a8f9c6a2b60972e24cf92e3aff8ac6d472baa2
vichu <vichu@vishakhavels-MacBook-Air.local>
2024-05-13 18:03:00 -0700

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import { headerLogo } from '../assets/images';
+import { hamburger } from '../assets/icons';
+
+import { navLinks } from '../constants';
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absoulte z-10 w-full'>
+      {/* using semantic tags for better accessibility */}
+      <nav className='flex justify-between items-start max-container'>
+        <a href='/'>
+          <img src={headerLogo} alt='logo' width={130} height={29} />
+        </a>
+
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray'
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        {/* hidden usually, but display block when screen is smaller */}
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger' width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Subscribe,
+  Services,
+  Footer,
+  CustomerReviews,
+  SpecialOffer,
+};
